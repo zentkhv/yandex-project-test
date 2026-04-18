059c96b3d21562bc0db1e61fd969c69472b4bc93
Zent <zentkhv27@gmail.com>
2026-04-18 13:08:04 +1000

    pylint.yml: Доработки

--- src/debug.py
+++ src/debug.py
@@ -1,11 +1,13 @@
+"""Модуль для отладки."""
 import json
 import sys
 
 if __name__ == "__main__":
     test_dict = {1:1, 2:2, 3:3}
 
-    for item in test_dict:
-        print(f"{item}: {test_dict[item]}")
+    for key, value in test_dict.items():
+
+        print(f"{key}: {value}")
 
     json_dict = json.dumps(test_dict, ensure_ascii=False, indent=4)
     print(json_dict)
