448cc5adade9eefd5de0f608f86bae80fdef6bf1
Zent <zentkhv27@gmail.com>
2026-04-26 21:09:36 +1000

    Ruff должен отработать успешно.

--- src/debug.py
+++ src/debug.py
@@ -1,15 +1,12 @@
 """Модуль для отладки."""
+
 import json
 import sys
 
 if __name__ == "__main__":
-    test_dict = {1: 1, 2: 2, 3:3}
-
-
-
+    test_dict = {1: 1, 2: 2, 3: 3}
 
     for key, value in test_dict.items():
-
         print(f"{key}: {value}")
 
     json_dict = json.dumps(test_dict, ensure_ascii=False, indent=4)
